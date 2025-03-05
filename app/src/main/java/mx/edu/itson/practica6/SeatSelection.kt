005eaeaf5b576ffa8cbe5f6a995b07924e0a156c
CharlyOwo81 <gamaliel.armenta245254@potros.itson.edu.mx>
2025-03-04 19:59:20 -0700

    Segundo commit - Branch Practica 7

---
+++ app/src/main/java/mx/edu/itson/practica6/SeatSelection.kt
@@ -0,0 +1,17 @@
+package mx.edu.itson.practica6
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SeatSelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_selection)
+
+
+
+    }
+}
